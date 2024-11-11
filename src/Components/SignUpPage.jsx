a650a1095f58750ccbc15aaba70c3bcb0a490df0
chandjamba <chandjamba2001@gmail.com>
2024-11-11 08:48:56 -0500

    TWITT-1 completed

--- src/Components/SignUpPage.jsx
+++ src/Components/SignUpPage.jsx
@@ -21,16 +21,13 @@ export default function SignUpPage() {
     const formData = new FormData(event.target);
     const formDataEntries = formData.entries();
     const formDataObject = Object.fromEntries(formDataEntries);
-    console.log(formDataObject, "formDataObject");
 
     // Email input validation function call. //
     const isValidEmail = validateEmailWithRegex(formDataObject.email);
-    console.log(isValidEmail);
     setEmailError(isValidEmail);
 
     //  Password input validation function call. //
     const isValidPassword = validatePasswordWithRegex(formDataObject.password);
-    console.log(isValidPassword);
     setPasswordError(isValidPassword);
 
     // Name input validation function call. //
@@ -41,7 +38,6 @@ export default function SignUpPage() {
       return;
     }
 
-    console.log(nameInputNoWhiteSpace, "name input");
     // Create AuthUser/ Account in appwrite. By given below method. //
   };
 
@@ -98,7 +94,41 @@ export default function SignUpPage() {
                 onClick={togglePassword}
                 type="button"
               >
-                {showPassword ? "🔓" : "🔒"}
+                {showPassword ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="lucide lucide-eye-off"
+                  >
+                    <path d="M10.733 5.076a10.744 10.744 0 0 1 11.205 6.575 1 1 0 0 1 0 .696 10.747 10.747 0 0 1-1.444 2.49" />
+                    <path d="M14.084 14.158a3 3 0 0 1-4.242-4.242" />
+                    <path d="M17.479 17.499a10.75 10.75 0 0 1-15.417-5.151 1 1 0 0 1 0-.696 10.75 10.75 0 0 1 4.446-5.143" />
+                    <path d="m2 2 20 20" />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="lucide lucide-eye"
+                  >
+                    <path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0" />
+                    <circle cx="12" cy="12" r="3" />
+                  </svg>
+                )}
               </button>
             </div>
             {!passwordError && (
