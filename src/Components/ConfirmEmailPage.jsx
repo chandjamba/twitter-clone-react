57fb482afe2aea61553407208cc3cc1ff0a57e9b
chandjamba <chandjamba2001@gmail.com>
2024-12-23 04:03:29 -0500

    working on twitt-4 and twitt-5

---
+++ src/Components/ConfirmEmailPage.jsx
@@ -0,0 +1,15 @@
+import "./confirmEmail.scss";
+import { useSearchParams } from "react-router-dom";
+import "../lib/appwrite/services/auth.service"
+
+export default function ConfirmEmail() {
+  const [params] = useSearchParams();
+  const userId = params.get("userId");
+  console.log(userId);
+  const secret = params.get("secret");
+  console.log(secret);
+
+    const verifyEmailHandler = await createUserVerification();
+    
+  return <div className="confirmEmail">confirm email</div>;
+}
