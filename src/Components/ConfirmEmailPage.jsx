4a81b9a852dc095ba17b4db48407d6ef824fb3bd
chandjamba <chandjamba2001@gmail.com>
2024-12-25 23:52:11 -0500

    fixed userVerification variable into useEffect

--- src/Components/ConfirmEmailPage.jsx
+++ src/Components/ConfirmEmailPage.jsx
@@ -1,13 +1,16 @@
 import "./confirmEmail.scss";
 import { useSearchParams } from "react-router-dom";
 import "../lib/appwrite/services/auth.service"
+import { useEffect } from "react";
 
 export default function ConfirmEmail() {
   const [params] = useSearchParams();
   const userId = params.get("userId");
   const secret = params.get("secret");
 
-    const verifyEmailHandler = await createUserVerification();
+  useEffect(async()=> {
+    await createUserVerification();
+  }, []);
     
   return <div className="confirmEmail">confirm email</div>;
 }
