9d56b5bf047a86944162dd35cbd625266bffceec
chandjamba <chandjamba2001@gmail.com>
2025-01-30 21:00:01 -0500

    fixed TWITT-9

--- src/Components/SignInPage.jsx
+++ src/Components/SignInPage.jsx
@@ -21,15 +21,6 @@ export default function SignInPage() {
     const formData = new FormData(event.target);
     const formDataEntries = formData.entries();
     const formDataObject = Object.fromEntries(formDataEntries);
-<<<<<<< HEAD
-    await authService.createSession({
-      email: formDataObject.email,
-      password: formDataObject.password,
-    });
-    
-
-=======
->>>>>>> main
     const isValidEmail = validateEmailWithRegex(formDataObject.email);
     setEmailError(!isValidEmail);
     //
