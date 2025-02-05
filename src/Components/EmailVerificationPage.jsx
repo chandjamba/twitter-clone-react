34a1e2b7db5bf77fbdcb452c9452e8c9dfeaae98
chandjamba <chandjamba2001@gmail.com>
2025-02-05 01:54:12 -0500

    Working on email verification page UI

--- src/Components/EmailVerificationPage.jsx
+++ src/Components/EmailVerificationPage.jsx
@@ -1,12 +1,15 @@
-import "./emailVerificationPage.scss";
+import "../styles/main.scss";
 import "../lib/appwrite/services/auth.service";
 import { authService } from "../lib/appwrite/services/auth.service";
 import { useNavigate, useSearchParams } from "react-router-dom";
+import { Twitter } from "lucide-react";
+import { useState } from "react";
 
 export default function EmailVerificationPage() {
   const [params] = useSearchParams();
   const userId = params.get("userId");
   const secret = params.get("secret");
+  // const [email, setEmail] = useState();
   const navigate = useNavigate();
 
   const verifyEmailHandler = async () => {
@@ -14,17 +17,34 @@ export default function EmailVerificationPage() {
     navigate("/signin");
   };
 
+  const onResendEmail = async() => {
+    await authService.createUserVerification(APPWRITE_CONFIG.sendVerificationEmailUrl);
+  };
+
   return (
-    <div className="verifyEmail">
-      <div className="verifyEmail__inner-box">
-        <div className="verifyEmail__para-box">
-          <p className="verifyEmail__para">
-            Please click the below button for Email verification.
-          </p>
-        </div>
-        <button className="verifyEmail__btn" onClick={verifyEmailHandler}>
-          Verify Email
+    <div className="email-verification-container">
+      <div className="email-verification-content">
+        <Twitter  className="twitter icon"/>
+        <h1>Verify your email</h1>
+        <p className="verification-text">
+          You're almost there! We sent an email to
+          <br />
+          <strong>{"email"}</strong>
+        </p>
+        <p className="verification-instruction">
+          Just click on the link in that email to complete your signup.
+          <br />
+          If you don't see it, you may need to <strong>check after few seconds.</strong>
+        </p>
+        <button className="verify-button" onClick={verifyEmailHandler}>
+          {"Verify email"}
         </button>
+        <p className="resend-text">
+          Didn't receive the email?{" "}
+          <button className="resend-button" onClick={onResendEmail}>
+            Click to resend
+          </button>
+        </p>
       </div>
     </div>
   );
