b4aedc56bcf373a687217c25c5501040239ba33e
chandjamba <chandjamba2001@gmail.com>
2025-02-05 20:28:33 -0500

    working on not found page styling

--- src/Components/NotFoundPage.jsx
+++ src/Components/NotFoundPage.jsx
@@ -1,4 +1,3 @@
-import "../styles/main.scss";
 import { Link } from "react-router-dom";
 import "./notFoundPage.scss";
 import { Twitter } from "lucide-react";
@@ -6,16 +5,16 @@ import { Twitter } from "lucide-react";
 export default function NotFoundPage() {
   return (
     <div className="container">
-      <div className="auth-container">
-    <div className="not-found-container">
-      <Twitter className="twitter-icon" />
-      <h1>404 - Page Not Found</h1>
-      <p className="not-found-text">
-        Hmm...this page doesn't exist. Try searching for something else.
-      </p>
-      <Link className="auth-button" to={"/signin"}>Back to Home</Link>
-    </div>
-    </div>
+      <div className="not-found-container">
+        <Twitter className="twitter-icon" />
+        <h1>404 - Page Not Found</h1>
+        <p className="not-found-text">
+          Hmm...this page doesn't exist. Try searching for something else.
+        </p>
+        <Link className="btn-primary" to={"/signin"}>
+          Back to Home
+        </Link>
+      </div>
     </div>
   );
 }
